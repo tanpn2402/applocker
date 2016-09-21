6c7e9aba78c960feca94bb642a216f3ee1ecb20d
tan <14520807@gm.uit.edu.vn>
2016-09-21 23:52:24 +0700

    them package fragment gom 2 fragment: AppsFragment + SettingsFragment, them package android.support.v4.preference, them layout/preference_list_fragment.xml, values/arrays.xml, xml/settings.xml; va con sua o cac cho khac nua

--- app/src/main/java/com/tanpn/applocker/MainActivity.java
+++ app/src/main/java/com/tanpn/applocker/MainActivity.java
@@ -3,6 +3,7 @@ package com.tanpn.applocker;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.tanpn.applocker.fragments.AppsFragment;
+import com.tanpn.applocker.fragments.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,14 +27,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,13 +76,22 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_all_apps) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+            AppsFragment appsFragment = new AppsFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(
+                    R.id.fragment_replace,
+                    appsFragment
+            ).commit();
+        } else if (id == R.id.nav_settings) {
+            SettingsFragment settinsFragment = new SettingsFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(
+                    R.id.fragment_replace,
+                    settinsFragment
+            ).commit();
+        } else if (id == R.id.nav_contact) {
 
         } else if (id == R.id.nav_share) {
 
---
+++ app/src/main/java/com/tanpn/applocker/fragments/SettingsFragment.java
@@ -0,0 +1,55 @@
+package com.tanpn.applocker.fragments;
+
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+
+import android.support.v4.app.Fragment;
+
+import android.support.v4.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tanpn.applocker.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+    private ListPreference mListPreference;
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle paramBundle) {
+        super.onCreate(paramBundle);
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        /*mListPreference = (ListPreference)  getPreferenceManager().findPreference("preference_key");
+        mListPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+                return false;
+            }
+        });
+*/
+        return super.onCreateView(inflater, container,
+                savedInstanceState);
+    }
+
+
+    @Override
+    public boolean onPreferenceTreeClick() {
+        return false;
+    }
+}
