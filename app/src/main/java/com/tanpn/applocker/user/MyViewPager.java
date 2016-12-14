e0dcefec250610d49282fac9034a4c16d72307cd
sanhkythach <thachkysanh@gmail.com>
2016-12-14 18:15:03 +0700

    hien thi nhom phan quyen, su dung sqlite

---
+++ app/src/main/java/com/tanpn/applocker/user/MyViewPager.java
@@ -0,0 +1,42 @@
+package com.tanpn.applocker.user;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by phamt_000 on 11/28/16.
+ */
+public class MyViewPager extends ViewPager {
+
+    private int mCurrentPagePosition = 0;
+
+    public MyViewPager(Context context) {
+        super(context);
+    }
+
+    public MyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        try {
+            View child = getChildAt(mCurrentPagePosition);
+            if (child != null) {
+                child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+                int h = child.getMeasuredHeight();
+                heightMeasureSpec = MeasureSpec.makeMeasureSpec(h, MeasureSpec.EXACTLY);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public void reMeasureCurrentPage(int position) {
+        mCurrentPagePosition = position;
+        requestLayout();
+    }
+}
